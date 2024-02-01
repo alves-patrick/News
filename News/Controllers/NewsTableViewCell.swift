8eebbbd9f869c9f627d972a7986482464e3403aa
Patrick Alves <patrickalvesarts@gmail.com>
2024-01-31 21:39:22 -0300

    Color

---
+++ News/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  News
+//
+//  Created by Patrick Alves on 1/30/24.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
